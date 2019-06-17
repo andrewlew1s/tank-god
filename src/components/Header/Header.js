f26464b2d924ce2321cbdc7dd0b99e1aab56a68e
unknown <andrew12lewis@gmail.com>
2019-06-16 22:49:56 -0700

    feat: basic page w/header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import HeaderContent from './HeaderContent'
+import './Header.css';
+
+const Header = () => {
+	return (
+		<header className="Header-wrap">
+			<div className="Header-img">
+				<div className="Header-content">
+					<HeaderContent />
+				</div>
+			</div>
+      </header>
+	)
+}
+
+export default Header;
---
+++ src/components/Header/HeaderContent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './HeaderContent.css';
+
+const headerContent = () => {
+	return (
+		<div >
+			<h1 className="text">TANK GOD</h1>
+			<img className="image" src="https://pbs.twimg.com/media/DlZ2k-6X0AEL9We.jpg" />
+		</div>
+	)
+}
+
+export default headerContent;
