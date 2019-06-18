5b5c1158d9bfc5658c7e95a6ba189972245b8e45
unknown <andrew12lewis@gmail.com>
2019-06-17 22:15:00 -0700

    feat: adventure corp + footer css managed

--- src/components/Landing/Footer/Footer.js
+++ src/components/Landing/Footer/Footer.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './Footer.css';
+import AdCorp from '../../../assets/adcorp.svg';
+import EthWhite from '../../../assets/eth-white.svg';
+
+
 
 const Footer = () => {
 	return (
-		<div className="Footer-wrap">
+		<div className="footer-wrap" >
 			<input className="input" placeholder="enter mobile #"/>
-     	</div>
+			<div className="img-wrap">
+				<img className="svg" src={AdCorp} alt="" />
+				<img className="svg" src={EthWhite} alt="" />
+			</div>
+		</div>
 	)
 }
 
