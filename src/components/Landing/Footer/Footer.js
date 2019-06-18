0122815e1a419304b71f49e4765e20886ff6314b
unknown <andrew12lewis@gmail.com>
2019-06-17 22:36:49 -0700

    feat: collect button

--- src/components/Landing/Footer/Footer.js
+++ src/components/Landing/Footer/Footer.js
@@ -7,8 +7,11 @@ import EthWhite from '../../../assets/eth-white.svg';
 
 const Footer = () => {
 	return (
-		<div className="footer-wrap" >
-			<input className="input" placeholder="enter mobile #"/>
+		<div>
+			<div className="footer-wrap" >
+				<input className="input" placeholder="enter mobile #"/>
+				<button className="button" type="button">Collect</button>
+			</div>
 			<div className="img-wrap">
 				<img className="svg" src={AdCorp} alt="" />
 				<img className="svg" src={EthWhite} alt="" />
